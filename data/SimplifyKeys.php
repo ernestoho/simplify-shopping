fd1fd338cb3281aa2c43f3b690ce786857476656
ernestoho <junior.ho@gmail.com>
2017-12-08 16:01:44 -0400

    Update SimplifyKeys.php

--- data/SimplifyKeys.php
+++ data/SimplifyKeys.php
@@ -27,7 +27,7 @@
  */
 
 /* TEST dummy account keys.  REPLACE them with your own Simplify.com API keys.*/
-$simplifyPrivateKey = 'DvO+9QRpinM6iBQI/OFxpbId/kYi9pCQ4petiZHZiCJ5YFFQL0ODSXAOkNtXTToq'; 
-$simplifyPublicKey = 'sbpb_M2M0ZTk2Y2ItNTcxMi00Y2QxLWJmNTctNzg4ZjEzMGMzY2Nj';
+$simplifyPrivateKey = 'vFwiUAMIPbiOhhCWfrXx5hJfgY2FXG6t3p6xFbfu7Wh5YFFQL0ODSXAOkNtXTToq'; 
+$simplifyPublicKey = 'sbpb_N2Y5ZGQ5NDctN2JkOC00OWY1LTkxMDctZmJhZTIyYWJkNWMy';
 
 ?>
